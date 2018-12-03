4cd8de1de0a06329f23ba458246a7d089c1f12ac
Ian Ansell <ian.ansell.uk@gmail.com>
2018-12-03 15:20:36 +0000

    pre-commit modulers built

---
+++ .githooks/pre-commit.d/02-key_files.py
@@ -0,0 +1,50 @@
+#!/usr/local/bin/python3
+
+import subprocess, sys, os, re, argparse
+
+BLACKLIST = [
+    b'BEGIN RSA PRIVATE KEY',
+    b'BEGIN DSA PRIVATE KEY',
+    b'BEGIN EC PRIVATE KEY',
+    b'BEGIN OPENSSH PRIVATE KEY',
+    b'BEGIN PRIVATE KEY',
+    b'PuTTY-User-Key-File-2',
+    b'BEGIN SSH2 ENCRYPTED PRIVATE KEY',
+    b'BEGIN PGP PRIVATE KEY BLOCK',
+]
+
+
+def detect_private_key(argv=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filenames', nargs='*', help='Filenames to check')
+    args = parser.parse_args(argv)
+
+    private_key_files = []
+
+    for filename in args.filenames:
+        with open(filename, 'rb') as f:
+            content = f.read()
+            if any(line in content for line in BLACKLIST):
+                private_key_files.append(filename)
+
+    if private_key_files:
+        for private_key_file in private_key_files:
+            print('Private key found: {}'.format(private_key_file))
+            sys.exit(1)
+    else:
+        return 0
+
+def main():
+    # Now, do the checking:
+    try:
+        detect_private_key()
+        print("No badness found")
+        sys.exit(0)
+
+    except subprocess.CalledProcessError:
+    	# There was a problem calling "git status".
+    	print("Oops...")
+    	sys.exit(12)
+
+if __name__ == '__main__':
+    main()
